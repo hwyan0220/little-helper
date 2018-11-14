bd4ed1f2f9694c58bb362e68445d87333d1fd52e
Norman Yan <hwyan0220@gmail.com>
2018-11-14 19:09:45 +1100

    Attempt to make firebase working for dialogflow

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,68 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+      // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+      return res.redirect(303, snapshot.ref.toString());
+    });
+  });
+
+
+exports.getResponse = functions.https.onRequest((req, res) =>{
+      // You can find your project ID in your Dialogflow agent settings
+const projectId = 'nextgenbot-33168'; //https://dialogflow.com/docs/agents#settings
+const sessionId = '12345';
+const query = 'hello';
+const languageCode = 'en-US';
+// Instantiate a DialogFlow client.
+const dialogflow = require('dialogflow');
+const sessionClient = new dialogflow.SessionsClient();
+
+// Define session path
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// The text query request.
+const request = {
+  session: sessionPath,
+  queryInput: {
+    text: {
+      text: query,
+      languageCode: languageCode,
+    },
+  },
+};
+
+// Send request and log result
+sessionClient
+  .detectIntent(request)
+  .then(responses => {
+
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+    return Response[0]; 
+  })
+  .catch(err => {
+    return err;
+  });
+
+
+});
+
+
